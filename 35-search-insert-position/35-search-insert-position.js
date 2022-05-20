7c066bda54c5a3e950eb974a97e5e80932fb558f
Ahmed Sayed <58530571+ahmedsayed621@users.noreply.github.com>
2022-05-20 10:53:32 +0200

    Time: 102 ms (14.49%), Space: 42.4 MB (39.24%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let index = 0;
+    
+    for(let i=0;i<nums.length;i++)
+        {
+            if(nums[i] ===  target)
+                {
+                    index= i
+                }
+            else if(nums[i] !== target)
+                {
+                    if(nums[i] < target)
+                        index=i+1
+                    
+                }
+        }
+    return index
+};
