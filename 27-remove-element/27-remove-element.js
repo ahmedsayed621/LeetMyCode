37ff1a95917263a2e9baef329edc57f82dd21163
Ahmed Sayed <58530571+ahmedsayed621@users.noreply.github.com>
2022-05-20 09:05:05 +0200

    Time: 111 ms (10.01%), Space: 41.9 MB (82.61%) - LeetHub

---
+++ 27-remove-element/27-remove-element.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    while (nums.includes(val)){
+        nums.splice(nums.indexOf(val),1);
+    }
+    var n  = nums.length;
+    return n 
+};
