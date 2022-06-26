741ccd0a0311b7440ba336aa895d28245b9732df
Ahmed Sayed <58530571+ahmedsayed621@users.noreply.github.com>
2022-06-26 18:38:53 +0200

    Time: 431 ms (13.77%), Space: 47 MB (54.63%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        
+        int minProfit=int.MaxValue;
+        int maxProfit=0;
+        
+        for(int i = 0 ; i<prices.Length;i++)
+        {
+            if(prices[i]<minProfit)
+                minProfit=prices[i];
+            
+            else if(prices[i]-minProfit > maxProfit)
+                maxProfit = prices[i]-minProfit;
+            
+        }
+        
+        return maxProfit;
+    }
+}
