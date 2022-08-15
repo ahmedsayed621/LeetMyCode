e1ac465a76932e2eab4fbcba88d5e31c43dc0a89
Ahmed Sayed <58530571+ahmedsayed621@users.noreply.github.com>
2022-08-15 08:07:52 +0200

    Time: 113 ms (49.96%), Space: 37 MB (35.83%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int RomanToInt(string s) {
+        if (s == null || s == string.Empty)
+            return 0;
+        
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+        int result = 0;
+        
+        dict.Add("I", 1);
+        dict.Add("V", 5);
+        dict.Add("X", 10);
+        dict.Add("L", 50);
+        dict.Add("C", 100);
+        dict.Add("D", 500);
+        dict.Add("M", 1000);
+        dict.Add("IV", 4);
+        dict.Add("IX", 9);
+        dict.Add("XL", 40);
+        dict.Add("XC", 90);
+        dict.Add("CD", 400);
+        dict.Add("CM", 900);
+        
+        for (int i = 0; i < s.Length; i++)
+            if ((s[i] == 'I' || s[i] == 'X' || s[i] == 'C') && i < s.Length - 1 && dict.ContainsKey(s.Substring(i, 2)))
+                result += dict[s.Substring(i++, 2)];
+            else
+                result += dict[s[i].ToString()];
+        
+        return result;
+    }
+}
